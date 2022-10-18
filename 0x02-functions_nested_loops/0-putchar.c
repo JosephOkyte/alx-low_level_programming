240628512f617ebbbbd1832a1adac03afccb59bb
JosephOkyte <josephokite@gmail.com>
2022-10-18 07:23:12 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
  * main - Program to print putchar
- * 
+ *
  * Description: Program to print putchar
  *
  * Return: return 0 (Success)
