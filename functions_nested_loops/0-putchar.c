f5986df3e81ade9a40a31a8eeb8aa02210e501ff
JosephOkyte <josephokite@gmail.com>
2022-10-18 04:36:04 -0700

    task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: 0
+ */
+
+int main() {
+
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+
+	return 0;
+}
