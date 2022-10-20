45e2bbb3134de86ec39c9a555edfe42d8f0cce8c
JosephOkyte <josephokite@gmail.com>
2022-10-20 14:55:26 -0700

    God abeg

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that prints
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num < 101; num++)
+	{
+		if (num % 15 == 0)
+			printf("FizzBuzz");
+		else if (num % 3 == 0)
+			printf("Fizz");
+		else if (num % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", num);
+
+		if (num != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
