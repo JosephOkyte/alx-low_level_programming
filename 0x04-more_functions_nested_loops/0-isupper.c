a2f58b08d801767610b75642db59d9a9a4a23e8a
JosephOkyte <josephokite@gmail.com>
2022-10-20 08:27:45 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character value
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && C <= 'Z')
+		return (1);
+	else
+		return (0);
+}
