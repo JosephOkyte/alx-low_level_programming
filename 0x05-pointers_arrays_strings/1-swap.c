dcb3a156840eefadde09db20ca51ec6c968c617c
JosephOkyte <josephokite@gmail.com>
2022-10-24 03:53:19 -0700

    e no go work

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: variable a
+ * @b: variable b
+ * Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int a;
+	int b;
+
+	swap_int(&a, &b);
+
+	return (0);
+}
