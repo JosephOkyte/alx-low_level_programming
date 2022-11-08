9ff73604c1d9078d788abd54f81dca223ef9f4c5
JosephOkyte <josephokite@gmail.com>
2022-11-08 04:37:58 -0800

    task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,12 +3,10 @@
 #include <stdlib.h>
 
 /**
- * print_grid - prints a grid of integers
- * @grid: the address of the two dimensional grid
- * @width: width of the grid
- * @height: height of the grid
- *
- * Return: Nothing.
+ * free_grid - 2D grid previously created
+ * @grid: pointer to a grid
+ * @height: rows in grid
+ * Return: Nothing
  */
 
 void free_grid(int **grid, int height)
